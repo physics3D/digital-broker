bf1fc3d3e7c7885cf07c8bd610940e776732c78c
physics3D <kroener.tobias@t-online.de>
2021-10-21 17:48:44 +0200

    add github link

--- src/App.svelte
+++ src/App.svelte
@@ -20,3 +20,14 @@
     <Setup />
   {/if}
 </main>
+<footer
+  class="fixed left-0 right-0 bottom-0 bg-gray-400 text-black text-center"
+>
+  <p>
+    See the source code on <a
+      href="https://github.com/physics3D/digital-broker"
+      class="underline text-blue-900"
+      target="_blank">Github</a
+    >
+  </p>
+</footer>
